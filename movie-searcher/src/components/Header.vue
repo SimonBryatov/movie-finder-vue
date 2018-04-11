3e06878afd1624db6a19c2b02459fd2d672bf227
Simon Bryatov <bsr-007@mail.ru>
2018-04-11 17:00:12 +0400

    hello

---
+++ movie-searcher/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="header"> {{msg}}
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      msg: 'Welcome to Your FIRST Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+.header {
+    color: darkred;
+    font-size: 300px;
+}
+</style>
